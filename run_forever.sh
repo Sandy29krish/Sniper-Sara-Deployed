a79beb36b99ba95d99133d4047be25ea24a9d4ef
Sandy29krish <sandyk29krish@gmail.com>
2025-07-18 05:47:31 +0530

    Update run_forever.sh

--- run_forever.sh
+++ run_forever.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
-while true; do
-  python runner.py
-  echo "Sniper bot crashed with exit code $?. Restarting..." >&2
-  sleep 5
+
+echo "Starting Sniper expiry strategy bot..."
+
+while true
+do
+  echo "Launching runner.py at $(date)"
+  python3 runner.py
+  echo "Restarting in 10 seconds after crash..."
+  sleep 10
 done
