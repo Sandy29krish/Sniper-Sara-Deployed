770e808ed63d7d902bba1a9127e8a6ab50e32a00
Sandy29krish <sandyk29krish@gmail.com>
2025-07-18 15:12:46 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,3 +4,4 @@ CAPITAL = 220000                           # Updated capital
 MAX_PREMIUM = 150                          # Max premium for OTM selection
 STRATEGY_MODE = "LIVE"                     # "LIVE" or "BACKTEST"
 SYMBOLS = ["BANKNIFTY", "NIFTY", "SENSEX"] # Indices to trade
+from config import TELEGRAM_BOT_TOKEN, TELEGRAM_CHAT_ID
